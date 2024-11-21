c22f8ca74bf133e3d6a00abec501f30b305cc79d
AgustinCNz <agustin_correanz@outlook.com>
2024-11-21 02:36:48 -0300

    realizando un nuevo inicio

---
+++ JavaScript/navbar.js
@@ -0,0 +1,9 @@
+/* funcion para el boton hamburguesa*/
+function toggleMenu() {
+    const menu = document.querySelector('.navbar-menu');
+    const toggleButton = document.querySelector('.menu-toggle');
+    menu.classList.toggle('active');
+    toggleButton.classList.toggle('active');
+  }
+  
+/* fin de la funcion para el boton hamburguesa*/
